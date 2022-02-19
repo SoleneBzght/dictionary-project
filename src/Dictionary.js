80e05c168d3676203495b8603e724752a1d3e866
SoleneBzght <solene.bouzag@gmail.com>
2022-02-19 23:59:59 +0100

    Added Photo API,
    Added Photo API, called whenever the keyword changes
    Added a photos state, null by default. Then Photos component receives the photos state as props
    Photos component receives the photos, makes sure there is any, and if true displays a section that loops through the photos found and displays 3 rows of pictures, with a maximum of 6.
    Each photo is a clickable link directing towards the original picture

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import Results from "./Results";
 import axios from "axios";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictioResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictioResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001cfc8460811ec448b9541a34d8e09f5f0";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -43,6 +55,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
