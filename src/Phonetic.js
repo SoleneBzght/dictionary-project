a1653cb54f4515e2f4c1f8d4ae799c0915fe24ec
SoleneBzght <solene.bouzag@gmail.com>
2022-02-20 00:18:32 +0100

    Edited Phonetics.js and css
    added condition: if there is no audio available the sound emoji is rendered, and can be click. If there is no audio, the "no sound" emoji is displayed and cannot be clicked.

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,12 +2,28 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
-        🔊
-      </a>{" "}
-      {props.phonetic.text}
-    </div>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        <a
+          href={props.phonetic.audio}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="Phonetic-true"
+        >
+          🔊
+        </a>{" "}
+        {props.phonetic.text}
+      </div>
+    );
+  } else {
+    return (
+      <div className="Phonetic">
+        <a href="#" className="Phonetic-false">
+          🔇
+        </a>{" "}
+        {props.phonetic.text}
+      </div>
+    );
+  }
 }
